1f57941429329584baf58cbe11b2e20e4c772c91
PashaGur <kalliak1993@yandex.ru>
2017-11-27 17:19:27 +0300

    initial commit

--- 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -1,16 +1,21 @@
 package com.javarush.task.task13.task1319;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /* 
 Писатель в файл с консоли
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        // напишите тут ваш код
+    public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(reader.readLine()));
+        String text;
+        do{
+            text=reader.readLine();
+            writer.write(text +"\n");
+        }while (!text.equals("exit"));
+
+        writer.close();
     }
 }
